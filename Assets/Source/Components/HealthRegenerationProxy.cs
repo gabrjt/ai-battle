237d3eb95f00c0f4fca48e155fe193e9abec20b7
Gabriel <gabr.j.t@gmail.com>
2019-02-24 16:03:40 -0300

    health systems

---
+++ Assets/Source/Components/HealthRegenerationProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Game.Components
+{
+    [Serializable]
+    public struct HealthRegeneration : IComponentData
+    {
+        public float Value;
+    }
+
+    public class HealthRegenerationProxy : ComponentDataProxy<HealthRegeneration> { }
+}
---
+++ Assets/Source/Systems/ClampHealthSystem.cs
@@ -0,0 +1,45 @@
+﻿using Game.Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace Game.Systems
+{
+    [UpdateAfter(typeof(HealthRegenerationSystem))]
+    [UpdateAfter(typeof(DamageSystem))]
+    public class ClampHealthSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        private struct ClampMaximumHealthJob : IJobProcessComponentData<Health, MaximumHealth>
+        {
+            public void Execute(ref Health health, [ReadOnly] ref MaximumHealth maximumHealth)
+            {
+                if (health.Value > maximumHealth.Value)
+                {
+                    health.Value = maximumHealth.Value;
+                }
+            }
+        }
+
+        [BurstCompile]
+        private struct ClampMinimumHealthJob : IJobProcessComponentData<Health>
+        {
+            public void Execute(ref Health health)
+            {
+                if (health.Value < 0)
+                {
+                    health.Value = 0;
+                }
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var clampMaximumHealthHandle = new ClampMaximumHealthJob { }.Schedule(this, inputDeps);
+            var clampMinimumHealthHandle = new ClampMinimumHealthJob { }.Schedule(this, clampMaximumHealthHandle);
+
+            return clampMinimumHealthHandle;
+        }
+    }
+}
