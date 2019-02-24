4478a4d8e58bbf6ce412d4f8caaedc43c973a0c6
Gabriel <gabr.j.t@gmail.com>
2019-02-24 16:30:58 -0300

    canvas singleton

---
+++ Assets/Source/Components/CanvasSingletonProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Game.Components
+{
+    [Serializable]
+    public struct CanvasSingleton : IComponentData
+    {
+        [HideInInspector]
+        public @bool Initialized;
+    }
+
+    public class CanvasSingletonProxy : ComponentDataProxy<CanvasSingleton> { }
+}
---
+++ Assets/Source/Systems/SetCanvasSingletonSystem.cs
@@ -0,0 +1,59 @@
+﻿using Game.Components;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Game.Systems
+{
+    public class SetCanvasSingletonSystem : ComponentSystem
+    {
+        private struct Initialized : ISystemStateComponentData { }
+
+        private ComponentGroup m_Group;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            m_Group = GetComponentGroup(new EntityArchetypeQuery
+            {
+                All = new[] { ComponentType.ReadOnly<CanvasSingleton>() },
+                None = new[] { ComponentType.ReadOnly<Initialized>() }
+            }, new EntityArchetypeQuery
+            {
+                All = new[] { ComponentType.ReadOnly<Initialized>() },
+                None = new[] { ComponentType.ReadOnly<CanvasSingleton>() }
+            });
+        }
+
+        protected override void OnUpdate()
+        {
+            var chunkArray = m_Group.CreateArchetypeChunkArray(Allocator.TempJob);
+            var entityType = GetArchetypeChunkEntityType();
+            var canvasSingletonType = GetArchetypeChunkComponentType<CanvasSingleton>();
+
+            for (var chunkIndex = 0; chunkIndex < chunkArray.Length; chunkIndex++)
+            {
+                var chunk = chunkArray[chunkIndex];
+
+                if (chunk.Has(canvasSingletonType))
+                {
+                    var entity = chunk.GetNativeArray(entityType)[0];
+                    var canvasSingleton = chunk.GetNativeArray(canvasSingletonType)[0];
+                    canvasSingleton.Initialized = true;
+
+                    PostUpdateCommands.AddComponent(entity, new Initialized());
+                    PostUpdateCommands.SetComponent(entity, canvasSingleton);
+
+                    SetSingleton(canvasSingleton);
+                }
+                else
+                {
+                    var entity = chunk.GetNativeArray(entityType)[0];
+                    PostUpdateCommands.RemoveComponent<Initialized>(entity);
+                }
+            }
+
+            chunkArray.Dispose();
+        }
+    }
+}
