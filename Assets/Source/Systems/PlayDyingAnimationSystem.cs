911dba47c9e7efad21b29bd39bed56c8296100be
Gabriel <gabr.j.t@gmail.com>
2019-02-25 22:48:54 -0300

    death animations

---
+++ Assets/Source/Systems/PlayDyingAnimationSystem.cs
@@ -0,0 +1,19 @@
+﻿using Game.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    public class PlayDyingAnimationSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            ForEach((ref ViewReference viewReference, ref Dead dead) =>
+            {
+                var animator = EntityManager.GetComponentObject<Animator>(viewReference.Value);
+                animator.speed = 1;
+                animator.Play("Dying");
+            });
+        }
+    }
+}
