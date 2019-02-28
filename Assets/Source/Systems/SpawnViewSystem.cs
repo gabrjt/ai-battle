eef99aa909b46e696fa6c8f9f12d62a46c463f63
Gabriel <gabr.j.t@gmail.com>
2019-02-27 21:26:26 -0300

    spawn view system jobified

--- Assets/Source/Systems/SpawnViewSystem.cs
+++ Assets/Source/Systems/SpawnViewSystem.cs
@@ -1,14 +1,107 @@
 ﻿using Game.Components;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Transforms;
 using UnityEngine;
 
 namespace Game.Systems
 {
-    //[DisableAutoCreation]
-    public class SpawnViewSystem : ComponentSystem
+    public class SpawnViewSystem : JobComponentSystem
     {
+        [BurstCompile]
+        private struct ConsolidateJob : IJobChunk
+        {
+            public NativeQueue<Entity>.Concurrent AddInitializedEntityQueue;
+
+            public NativeQueue<Entity>.Concurrent RemoveInitializedEntityQueue;
+
+            public NativeQueue<SpawnData>.Concurrent SpawnDataQueue;
+
+            [ReadOnly]
+            public ArchetypeChunkEntityType EntityType;
+
+            [ReadOnly]
+            public ArchetypeChunkComponentType<Initialized> InitializedType;
+
+            [ReadOnly]
+            public ArchetypeChunkComponentType<Knight> KnightType;
+
+            [ReadOnly]
+            public ArchetypeChunkComponentType<OrcWolfRider> OrcWolfRiderType;
+
+            [ReadOnly]
+            public ArchetypeChunkComponentType<Skeleton> SkeletonType;
+
+            public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+            {
+                var entityArray = chunk.GetNativeArray(EntityType);
+
+                var initialized = chunk.Has(InitializedType);
+                var isKnight = chunk.Has(KnightType);
+                var isOrcWolfRider = chunk.Has(OrcWolfRiderType);
+                var isSkeleton = chunk.Has(SkeletonType);
+                var viewType = default(ViewType);
+
+                for (var entityIndex = 0; entityIndex < chunk.Count; entityIndex++)
+                {
+                    var entity = entityArray[entityIndex];
+
+                    if (!initialized)
+                    {
+                        AddInitializedEntityQueue.Enqueue(entity);
+
+                        if (isKnight)
+                        {
+                            viewType = ViewType.Knight;
+                        }
+                        else if (isOrcWolfRider)
+                        {
+                            viewType = ViewType.OrcWolfRider;
+                        }
+                        else if (isSkeleton)
+                        {
+                            viewType = ViewType.Skeleton;
+                        }
+
+                        SpawnDataQueue.Enqueue(new SpawnData
+                        {
+                            Owner = entity,
+                            ViewType = viewType
+                        });
+                    }
+                    else
+                    {
+                        RemoveInitializedEntityQueue.Enqueue(entity);
+                    }
+                }
+            }
+        }
+
+        private struct ApplyJob : IJob
+        {
+            public NativeQueue<Entity> AddInitializedEntityQueue;
+
+            public NativeQueue<Entity> RemoveInitializedEntityQueue;
+
+            [ReadOnly]
+            public EntityCommandBuffer EntityCommandBuffer;
+
+            public void Execute()
+            {
+                while (AddInitializedEntityQueue.TryDequeue(out var entity))
+                {
+                    EntityCommandBuffer.AddComponent(entity, new Initialized());
+                }
+
+                while (RemoveInitializedEntityQueue.TryDequeue(out var entity))
+                {
+                    EntityCommandBuffer.RemoveComponent<Initialized>(entity);
+                }
+            }
+        }
+
         private enum ViewType
         {
             Knight,
@@ -33,7 +126,11 @@ namespace Game.Systems
 
         private GameObject m_SkeletonPrefab;
 
-        private NativeList<SpawnData> m_SpawnDataList;
+        private NativeQueue<Entity> m_AddInitializedEntityQueue;
+
+        private NativeQueue<Entity> m_RemoveInitializedEntityQueue;
+
+        private NativeQueue<SpawnData> m_SpawnDataQueue;
 
         protected override void OnCreateManager()
         {
@@ -43,10 +140,10 @@ namespace Game.Systems
             {
                 All = new[] { ComponentType.ReadOnly<Character>(), ComponentType.ReadOnly<Position>(), ComponentType.ReadOnly<Rotation>() },
                 Any = new[] { ComponentType.ReadOnly<Knight>(), ComponentType.ReadOnly<OrcWolfRider>(), ComponentType.ReadOnly<Skeleton>() },
-                None = new[] { ComponentType.ReadOnly<Initialized>(), ComponentType.ReadOnly<Dead>() }
+                None = new[] { ComponentType.Create<Initialized>(), ComponentType.ReadOnly<Dead>() }
             }, new EntityArchetypeQuery
             {
-                All = new[] { ComponentType.ReadOnly<Initialized>() },
+                All = new[] { ComponentType.Create<Initialized>() },
                 None = new[] { ComponentType.ReadOnly<Character>(), ComponentType.ReadOnly<Position>(), ComponentType.ReadOnly<Rotation>() }
             });
 
@@ -54,77 +151,42 @@ namespace Game.Systems
             Debug.Assert(m_OrvWolfRiderPrefab = Resources.Load<GameObject>("Orc Wolf Rider"));
             Debug.Assert(m_SkeletonPrefab = Resources.Load<GameObject>("Skeleton"));
 
-            m_SpawnDataList = new NativeList<SpawnData>(Allocator.Persistent);
+            m_AddInitializedEntityQueue = new NativeQueue<Entity>(Allocator.Persistent);
+            m_RemoveInitializedEntityQueue = new NativeQueue<Entity>(Allocator.Persistent);
+            m_SpawnDataQueue = new NativeQueue<SpawnData>(Allocator.Persistent);
 
             RequireSingletonForUpdate<CameraSingleton>();
         }
 
-        protected override void OnUpdate()
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            if (!HasSingleton<CameraSingleton>()) return; // TODO: remove this when RequireSingletonForUpdate is working.
+            if (!HasSingleton<CameraSingleton>()) return inputDeps; // TODO: remove this when RequireSingletonForUpdate is working.
 
-            var chunkArray = m_Group.CreateArchetypeChunkArray(Allocator.TempJob);
-            var entityType = GetArchetypeChunkEntityType();
-            var initializedType = GetArchetypeChunkComponentType<Initialized>(true);
-            var knightType = GetArchetypeChunkComponentType<Knight>(true);
-            var orcWolfRiderType = GetArchetypeChunkComponentType<OrcWolfRider>(true);
-            var skeletonType = GetArchetypeChunkComponentType<Skeleton>(true);
+            var barrier = World.GetExistingManager<EndFrameBarrier>();
 
-            for (int chunkIndex = 0; chunkIndex < chunkArray.Length; chunkIndex++)
+            inputDeps = new ConsolidateJob
             {
-                var chunk = chunkArray[chunkIndex];
-                var entityArray = chunk.GetNativeArray(entityType);
+                AddInitializedEntityQueue = m_AddInitializedEntityQueue.ToConcurrent(),
+                RemoveInitializedEntityQueue = m_RemoveInitializedEntityQueue.ToConcurrent(),
+                SpawnDataQueue = m_SpawnDataQueue.ToConcurrent(),
+                EntityType = GetArchetypeChunkEntityType(),
+                InitializedType = GetArchetypeChunkComponentType<Initialized>(),
+                KnightType = GetArchetypeChunkComponentType<Knight>(true),
+                OrcWolfRiderType = GetArchetypeChunkComponentType<OrcWolfRider>(true),
+                SkeletonType = GetArchetypeChunkComponentType<Skeleton>(true)
+            }.Schedule(m_Group, inputDeps);
 
-                if (!chunk.Has(initializedType))
-                {
-                    for (int entityIndex = 0; entityIndex < chunk.Count; entityIndex++)
-                    {
-                        var entity = entityArray[entityIndex];
-
-                        PostUpdateCommands.AddComponent(entity, new Initialized());
-
-                        if (chunk.Has(knightType))
-                        {
-                            m_SpawnDataList.Add(new SpawnData
-                            {
-                                Owner = entity,
-                                ViewType = ViewType.Knight,
-                            });
-                        }
-                        else if (chunk.Has(orcWolfRiderType))
-                        {
-                            m_SpawnDataList.Add(new SpawnData
-                            {
-                                Owner = entity,
-                                ViewType = ViewType.OrcWolfRider,
-                            });
-                        }
-                        else if (chunk.Has(skeletonType))
-                        {
-                            m_SpawnDataList.Add(new SpawnData
-                            {
-                                Owner = entity,
-                                ViewType = ViewType.Skeleton,
-                            });
-                        }
-                    }
-                }
-                else
-                {
-                    for (int entityIndex = 0; entityIndex < chunk.Count; entityIndex++)
-                    {
-                        var entity = entityArray[entityIndex];
-                        PostUpdateCommands.RemoveComponent<Initialized>(entity);
-                    }
-                }
-            }
-
-            chunkArray.Dispose();
-
-            for (int i = 0; i < m_SpawnDataList.Length; i++)
+            inputDeps = new ApplyJob
             {
-                var spawnData = m_SpawnDataList[i];
+                AddInitializedEntityQueue = m_AddInitializedEntityQueue,
+                RemoveInitializedEntityQueue = m_RemoveInitializedEntityQueue,
+                EntityCommandBuffer = barrier.CreateCommandBuffer()
+            }.Schedule(inputDeps);
 
+            inputDeps.Complete();
+
+            while (m_SpawnDataQueue.TryDequeue(out var spawnData))
+            {
                 var owner = spawnData.Owner;
 
                 var position = EntityManager.GetComponentData<Position>(owner).Value;
@@ -162,16 +224,28 @@ namespace Game.Systems
                 EntityManager.AddComponentData(owner, new ViewReference { Value = entity });
             }
 
-            m_SpawnDataList.Clear();
+            barrier.AddJobHandleForProducer(inputDeps);
+
+            return inputDeps;
         }
 
         protected override void OnDestroyManager()
         {
             base.OnDestroyManager();
 
-            if (m_SpawnDataList.IsCreated)
+            if (m_AddInitializedEntityQueue.IsCreated)
             {
-                m_SpawnDataList.Dispose();
+                m_AddInitializedEntityQueue.Dispose();
+            }
+
+            if (m_RemoveInitializedEntityQueue.IsCreated)
+            {
+                m_RemoveInitializedEntityQueue.Dispose();
+            }
+
+            if (m_SpawnDataQueue.IsCreated)
+            {
+                m_SpawnDataQueue.Dispose();
             }
         }
     }
