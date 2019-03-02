3608d788ceed07e0258113b8432a4bc5d097fcb5
Gabriel <gabr.j.t@gmail.com>
2019-03-02 17:11:46 -0300

    removed useless check

--- Assets/Source/Systems/RotateTowardsTargetSystem.cs
+++ Assets/Source/Systems/RotateTowardsTargetSystem.cs
@@ -8,19 +8,16 @@ using Unity.Transforms;
 
 namespace Game.Systems
 {
-    
     public class RotateTowardsTargetSystem : JobComponentSystem
     {
         [BurstCompile]
-        struct Job : IJobProcessComponentData<Position, Target, Rotation>
+        private struct Job : IJobProcessComponentData<Position, Target, Rotation>
         {
             [ReadOnly]
             public ComponentDataFromEntity<Position> PositionFromEntity;
 
             public void Execute([ReadOnly] ref Position position, [ReadOnly] ref Target target, ref Rotation rotation)
             {
-                if (!PositionFromEntity.Exists(target.Value)) return;
-
                 rotation.Value = quaternion.LookRotation(PositionFromEntity[target.Value].Value - position.Value, math.up());
             }
         }
