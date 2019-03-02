11a7c402dc05e5349e49f46af32a61b6cd2b3e73
Gabriel <gabr.j.t@gmail.com>
2019-03-02 15:56:03 -0300

    character count input field

---
+++ Assets/Source/Systems/SetCharacterCountInputFieldSingletonSystem.cs
@@ -0,0 +1,60 @@
+﻿using Game.Components;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Game.Systems
+{
+    [UpdateInGroup(typeof(SetBarrier))]
+    public class SetCharacterCountInputFieldSingletonSystem : ComponentSystem
+    {
+        private struct Initialized : ISystemStateComponentData { }
+
+        private ComponentGroup m_Group;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            m_Group = GetComponentGroup(new EntityArchetypeQuery
+            {
+                All = new[] { ComponentType.ReadOnly<CharacterCountInputField>() },
+                None = new[] { ComponentType.ReadOnly<Initialized>() }
+            }, new EntityArchetypeQuery
+            {
+                All = new[] { ComponentType.ReadOnly<Initialized>() },
+                None = new[] { ComponentType.ReadOnly<CharacterCountInputField>() }
+            });
+        }
+
+        protected override void OnUpdate()
+        {
+            var chunkArray = m_Group.CreateArchetypeChunkArray(Allocator.TempJob);
+            var entityType = GetArchetypeChunkEntityType();
+            var characterCountInputFieldType = GetArchetypeChunkComponentType<CharacterCountInputField>();
+
+            for (var chunkIndex = 0; chunkIndex < chunkArray.Length; chunkIndex++)
+            {
+                var chunk = chunkArray[chunkIndex];
+
+                if (chunk.Has(characterCountInputFieldType))
+                {
+                    var entity = chunk.GetNativeArray(entityType)[0];
+                    var characterCountInputField = chunk.GetNativeArray(characterCountInputFieldType)[0];
+                    characterCountInputField.Owner = entity;
+
+                    PostUpdateCommands.AddComponent(entity, new Initialized());
+                    PostUpdateCommands.SetComponent(entity, characterCountInputField);
+
+                    SetSingleton(characterCountInputField);
+                }
+                else
+                {
+                    var entity = chunk.GetNativeArray(entityType)[0];
+                    PostUpdateCommands.RemoveComponent<Initialized>(entity);
+                }
+            }
+
+            chunkArray.Dispose();
+        }
+    }
+}
