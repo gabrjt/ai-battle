10681709453e14611886e396e07ab0068ec66ac3
Gabriel <gabr.j.t@gmail.com>
2019-03-02 16:04:31 -0300

    quit button

---
+++ Assets/Source/Behaviours/QuitBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Behaviours
+{
+    public class QuitBehaviour : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
