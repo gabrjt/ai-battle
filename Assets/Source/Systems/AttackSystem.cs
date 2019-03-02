8c7bcc070da0c75f2b7b51709bd121662a9c69af
Gabriel <gabr.j.t@gmail.com>
2019-03-02 16:14:41 -0300

    attack system refactor

--- Assets/Source/Systems/AttackSystem.cs
+++ Assets/Source/Systems/AttackSystem.cs
@@ -1,4 +1,6 @@
 ﻿using Game.Components;
+using System;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -6,32 +8,84 @@ using UnityEngine;
 
 namespace Game.Systems
 {
-    public class AttackSystem : JobComponentSystem
+    public class AttackSystem : JobComponentSystem, IDisposable
     {
-        public struct Job : IJobProcessComponentDataWithEntity<Attacking>
+        [BurstCompile]
+        public struct ConsolidateJob : IJobProcessComponentDataWithEntity<Attacking>
         {
-            public EntityCommandBuffer.Concurrent EntityCommandBuffer;
+            public NativeQueue<Entity>.Concurrent EntityQueue;
 
+            [ReadOnly]
             public float Time;
 
             public void Execute(Entity entity, int index, [ReadOnly] ref Attacking attack)
             {
                 if (attack.StartTime + attack.Duration > Time) return;
 
-                EntityCommandBuffer.RemoveComponent<Attacking>(index, entity);
+                EntityQueue.Enqueue(entity);
             }
         }
 
-        [Inject]
-        private EndFrameBarrier m_EndFrameBarrier;
+        private struct ApplyJob : IJob
+        {
+            public NativeQueue<Entity> EntityQueue;
+
+            public EntityCommandBuffer CommandBuffer;
+
+            public void Execute()
+            {
+                while (EntityQueue.TryDequeue(out var entity))
+                {
+                    CommandBuffer.RemoveComponent<Attacking>(entity);
+                }
+            }
+        }
+
+        private NativeQueue<Entity> m_EntityQueue;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            m_EntityQueue = new NativeQueue<Entity>(Allocator.Persistent);
+        }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            return new Job
+            m_EntityQueue.Clear();
+
+            var barrier = World.GetExistingManager<RemoveBarrier>();
+
+            inputDeps = new ConsolidateJob
             {
-                EntityCommandBuffer = m_EndFrameBarrier.CreateCommandBuffer().ToConcurrent(),
+                EntityQueue = m_EntityQueue.ToConcurrent(),
                 Time = Time.time
             }.Schedule(this, inputDeps);
+
+            inputDeps = new ApplyJob
+            {
+                EntityQueue = m_EntityQueue,
+                CommandBuffer = barrier.CreateCommandBuffer(),
+            }.Schedule(inputDeps);
+
+            barrier.AddJobHandleForProducer(inputDeps);
+
+            return inputDeps;
+        }
+
+        protected override void OnDestroyManager()
+        {
+            base.OnDestroyManager();
+
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (m_EntityQueue.IsCreated)
+            {
+                m_EntityQueue.Dispose();
+            }
         }
     }
 }
