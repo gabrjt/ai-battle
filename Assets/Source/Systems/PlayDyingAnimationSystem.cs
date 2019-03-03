8794b9837fa5d6c7c777c5a301aec27fff53996f
Gabriel <gabr.j.t@gmail.com>
2019-03-03 18:22:37 -0300

    play dying animation jobified

--- Assets/Source/Systems/PlayDyingAnimationSystem.cs
+++ Assets/Source/Systems/PlayDyingAnimationSystem.cs
@@ -1,19 +1,90 @@
 ﻿using Game.Components;
+using System;
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using UnityEngine;
 
 namespace Game.Systems
 {
-    public class PlayDyingAnimationSystem : ComponentSystem
+    public class PlayDyingAnimationSystem : JobComponentSystem, IDisposable
     {
-        protected override void OnUpdate()
+        [BurstCompile]
+        private struct ConsolidateJob : IJobChunk
         {
-            ForEach((ref ViewReference viewReference, ref Dead dead) =>
+            public NativeQueue<Entity>.Concurrent PlayDyingAnimationQueue;
+
+            [ReadOnly]
+            public ArchetypeChunkComponentType<ViewReference> ViewReferenceType;
+
+            [ReadOnly]
+            public ComponentDataFromEntity<Visible> VisibleFromEntity;
+
+            public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
             {
-                var animator = EntityManager.GetComponentObject<Animator>(viewReference.Value);
+                var viewReferenceArray = chunk.GetNativeArray(ViewReferenceType);
+                for (int entityIndex = 0; entityIndex < chunk.Count; entityIndex++)
+                {
+                    var entity = viewReferenceArray[entityIndex].Value;
+
+                    if (!VisibleFromEntity.Exists(entity)) continue;
+
+                    PlayDyingAnimationQueue.Enqueue(entity);
+                }
+            }
+        }
+
+        private ComponentGroup m_Group;
+
+        private NativeQueue<Entity> m_PlayDyingAnimationQueue;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            m_Group = GetComponentGroup(new EntityArchetypeQuery
+            {
+                All = new[] { ComponentType.ReadOnly<ViewReference>(), ComponentType.ReadOnly<Dead>() }
+            });
+
+            m_PlayDyingAnimationQueue = new NativeQueue<Entity>(Allocator.Persistent);
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            inputDeps = new ConsolidateJob
+            {
+                PlayDyingAnimationQueue = m_PlayDyingAnimationQueue.ToConcurrent(),
+                ViewReferenceType = GetArchetypeChunkComponentType<ViewReference>(true),
+                VisibleFromEntity = GetComponentDataFromEntity<Visible>(true)
+            }.Schedule(m_Group, inputDeps);
+
+            inputDeps.Complete();
+
+            while (m_PlayDyingAnimationQueue.TryDequeue(out var entity))
+            {
+                var animator = EntityManager.GetComponentObject<Animator>(entity);
                 animator.speed = 1;
                 animator.Play("Dying");
-            });
+            }
+
+            return inputDeps;
+        }
+
+        protected override void OnDestroyManager()
+        {
+            base.OnDestroyManager();
+
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (m_PlayDyingAnimationQueue.IsCreated)
+            {
+                m_PlayDyingAnimationQueue.Dispose();
+            }
         }
     }
 }
