338e15fdf9301463c34ca812c44a584f28ffd729
Gabriel <gabr.j.t@gmail.com>
2019-03-04 05:37:57 -0300

    refactor

--- Assets/Source/Systems/TargetBufferSystem.cs
+++ Assets/Source/Systems/TargetBufferSystem.cs
@@ -1,6 +1,6 @@
-﻿using Game.Comparers;
-using Game.Components;
-using System;
+﻿using Game.Components;
+using System.Linq;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -9,20 +9,86 @@ using Unity.Transforms;
 
 namespace Game.Systems
 {
-    [UpdateInGroup(typeof(SetBarrier))]
-    public class TargetBufferSystem : ComponentSystem
+    public class TargetBufferSystem : JobComponentSystem
     {
+        //[BurstCompile]
         private struct ConsolidateJob : IJobChunk
         {
+            [ReadOnly]
+            public ArchetypeChunkEntityType EntityType;
+
+            [ReadOnly]
+            public ComponentDataFromEntity<Dead> DeadFromEntity;
+
+            [ReadOnly]
+            public ComponentDataFromEntity<Position> PositionFromEntity;
+
+            [ReadOnly]
+            public ComponentDataFromEntity<AttackDistance> AttackDistanceFromEntity;
+
+            [NativeDisableParallelForRestriction]
+            public BufferFromEntity<TargetBufferElement> TargetBufferFromEntity;
+
             public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
             {
-                throw new NotImplementedException();
+                var entityArray = chunk.GetNativeArray(EntityType);
+
+                for (var entityIndex = 0; entityIndex < chunk.Count; entityIndex++)
+                {
+                    var entity = entityArray[entityIndex];
+                    var position = PositionFromEntity[entity].Value;
+
+                    var targetBuffer = TargetBufferFromEntity[entity];
+
+                    if (targetBuffer.Length == 0) continue;
+
+                    var targetBufferArray = targetBuffer.AsNativeArray();
+                    var threatTargetList = new NativeList<TargetBufferElement>(Allocator.Temp);
+
+                    for (var targetBufferIndex = 0; targetBufferIndex < targetBufferArray.Length; targetBufferIndex++)
+                    {
+                        var target = targetBufferArray[targetBufferIndex].Value;
+                        var targetPostion = PositionFromEntity[target].Value;
+
+                        if (DeadFromEntity.Exists(target) || math.distance(position, targetPostion) > AttackDistanceFromEntity[entity].Max) continue;
+
+                        threatTargetList.Add(target);
+                    }
+
+                    targetBufferArray = threatTargetList.AsArray();
+                    var targetBufferSort = new NativeArray<SortData>(targetBufferArray.Length, Allocator.Temp);
+
+                    for (int bufferIndex = 0; bufferIndex < targetBufferArray.Length; bufferIndex++)
+                    {
+                        var target = targetBufferArray[bufferIndex].Value;
+                        var targetPosition = PositionFromEntity[target].Value;
+                        targetBufferSort[bufferIndex] = new SortData
+                        {
+                            Target = target,
+                            Position = targetPosition
+                        };
+                    }
+
+                    targetBuffer.Clear();
+
+                    // TODO: Burst compliant sort algorithm 
+                    var targetBufferRange = new NativeArray<TargetBufferElement>(targetBufferSort.OrderBy((data) => math.distancesq(data.Position, position)).Select((data) => data.Target).ToArray(), Allocator.Temp);
+
+                    targetBuffer.AddRange(targetBufferRange);
+
+                    targetBufferRange.Dispose();
+                }
             }
         }
 
-        private ComponentGroup m_Group;
+        private struct SortData
+        {
+            public TargetBufferElement Target;
 
-        private TargetBufferComparer m_TargetBufferComparer; // TODO: make it blitable and thread safe
+            public float3 Position;
+        }
+
+        private ComponentGroup m_Group;
 
         protected override void OnCreateManager()
         {
@@ -33,51 +99,18 @@ namespace Game.Systems
                 All = new[] { ComponentType.ReadOnly<TargetBufferElement>() },
                 None = new[] { ComponentType.ReadOnly<Dead>() },
             });
-
-            m_TargetBufferComparer = new TargetBufferComparer();
-
-            RequireForUpdate(m_Group);
         }
 
-        protected override void OnUpdate()
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            var entityArray = m_Group.ToEntityArray(Allocator.TempJob);
-            var positionFromEntity = GetComponentDataFromEntity<Position>(true);
-            var deadFromEntity = GetComponentDataFromEntity<Dead>(true);
-            var attackDistanceFromEntity = GetComponentDataFromEntity<AttackDistance>(true);
-
-            for (var entityIndex = 0; entityIndex < entityArray.Length; entityIndex++)
+            return inputDeps = new ConsolidateJob
             {
-                var entity = entityArray[entityIndex];
-
-                var targetBuffer = EntityManager.GetBuffer<TargetBufferElement>(entity);
-
-                if (targetBuffer.Length == 0) continue;
-
-                var targetBufferArray = targetBuffer.AsNativeArray().ToArray();
-
-                var position = positionFromEntity[entity].Value;
-
-                m_TargetBufferComparer.Position = position;
-                m_TargetBufferComparer.PositionFromEntity = positionFromEntity;
-
-                Array.Sort(targetBufferArray, 0, targetBufferArray.Length, m_TargetBufferComparer);
-
-                targetBuffer.Clear();
-
-                var count = 0;
-                do
-                {
-                    var target = targetBufferArray[count++].Value;
-                    var targetPostion = positionFromEntity[target].Value;
-
-                    if (deadFromEntity.Exists(target) || math.distance(position, targetPostion) > attackDistanceFromEntity[entity].Max) continue;
-
-                    targetBuffer.Add(target);
-                } while (count < targetBufferArray.Length);
-            }
-
-            entityArray.Dispose();
+                EntityType = GetArchetypeChunkEntityType(),
+                PositionFromEntity = GetComponentDataFromEntity<Position>(true),
+                DeadFromEntity = GetComponentDataFromEntity<Dead>(true),
+                AttackDistanceFromEntity = GetComponentDataFromEntity<AttackDistance>(true),
+                TargetBufferFromEntity = GetBufferFromEntity<TargetBufferElement>()
+            }.Schedule(m_Group, inputDeps);
         }
     }
 }
