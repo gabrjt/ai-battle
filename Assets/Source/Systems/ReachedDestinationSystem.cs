a97edb6e83ccdeb55b5e307c11b72dc8ad26384f
Gabriel <gabr.j.t@gmail.com>
2019-03-08 03:09:34 -0300

    reached destination system

---
+++ Assets/Source/Systems/ReachedDestinationSystem.cs
@@ -0,0 +1,65 @@
+﻿using Game.Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    [UpdateInGroup(typeof(GameLogicGroup))]
+    [UpdateBefore(typeof(IdleSystem))]
+    public class ReachedDestinationSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        private struct ProcessJob : IJobProcessComponentDataWithEntity<Translation, Destination>
+        {
+            public NativeQueue<Entity>.Concurrent ProcessedQueue;
+
+            public void Execute(Entity entity, int index, [ReadOnly] ref Translation translation, [ReadOnly] ref Destination destination)
+            {
+                if (math.distancesq(translation.Value, destination.Value) > 0.01f) return;
+
+                ProcessedQueue.Enqueue(entity);
+            }
+        }
+
+        private NativeQueue<Entity> m_ProcessedQueue;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            m_ProcessedQueue = new NativeQueue<Entity>(Allocator.Persistent);
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            inputDeps = new ProcessJob
+            {
+                ProcessedQueue = m_ProcessedQueue.ToConcurrent()
+            }.Schedule(this, inputDeps);
+
+            inputDeps.Complete();
+
+            while (m_ProcessedQueue.TryDequeue(out var entity))
+            {
+                EntityManager.RemoveComponent<Destination>(entity);
+            }
+
+            return inputDeps;
+        }
+
+        protected override void OnDestroyManager()
+        {
+            base.OnDestroyManager();
+
+            if (m_ProcessedQueue.IsCreated)
+            {
+                m_ProcessedQueue.Dispose();
+            }
+        }
+    }
+}
