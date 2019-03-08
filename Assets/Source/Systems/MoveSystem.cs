1ce87d16e4a732225eaa89be83b21985a8fc9ac3
Gabriel <gabr.j.t@gmail.com>
2019-03-08 02:51:02 -0300

    move system

---
+++ Assets/Source/Systems/MoveSystem.cs
@@ -0,0 +1,33 @@
+﻿using Game.Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    [UpdateInGroup(typeof(GameLogicGroup))]
+    public class MoveSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        private struct MoveJob : IJobProcessComponentData<Velocity, Translation>
+        {
+            [ReadOnly] public float DeltaTime;
+
+            public void Execute([ReadOnly] ref Velocity velocity, ref Translation translation)
+            {
+                translation.Value += velocity.Value * DeltaTime;
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            return new MoveJob
+            {
+                DeltaTime = Time.deltaTime
+            }.Schedule(this, inputDeps);
+        }
+    }
+}
